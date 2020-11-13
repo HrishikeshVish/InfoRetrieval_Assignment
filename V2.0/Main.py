e6b3c397c587d9f38e28678ca25fd3e9f7bf6546
Hrishikesh Viswanath <38471846+HrishikeshVish@users.noreply.github.com>
2020-11-13 20:00:06 +0530

    Fixed wildcard issues

--- V2.0/Main.py
+++ V2.0/Main.py
@@ -4,5 +4,9 @@ query = input("Enter the search term")
 document_list = search(query)
 print(document_list)
 ranked_order_of_docs = rankByFreq(document_list)
-rankByTFIDF(ranked_order_of_docs, 5, query)
+if "*" in query or "?" in query:
+    ranked_docs = ranked_order_of_docs[0:10]
+else:
+    ranked_docs = rankByTFIDF(ranked_order_of_docs, 5, query)[0:10]
+
 
